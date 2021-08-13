69a2bad4bca5120b121108acafdd4588c8249eea
劉品均 <encoreu63990@gmail.com>
2021-08-14 00:15:34 +0800

    771_Jewels and Stones

---
+++ Algorithms/Problems/Easy/771_Jewels and Stones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/jewels-and-stones/
+    /// 
+    /// Case 1:
+    /// Input: jewels = "aA", stones = "aAAbbbb"
+    /// Output: 3
+    ///  
+    /// Case 2:
+    /// Input: jewels = "z", stones = "ZZ"
+    /// Output: 0
+    /// 
+    /// </summary>
+    class _771_Jewels_and_Stones
+    {
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            return stones.Where(c => jewels.Contains(c)).Count();
+        }
+    }
+}

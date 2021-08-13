6bf76a4b9bca648016ff4078e09e8b35b40ccf1e
劉品均 <encoreu63990@gmail.com>
2021-08-13 23:56:23 +0800

    1689_Partitioning Into Minimum Number Of Deci-Binary Numbers

---
+++ Algorithms/Problems/Medium/1689_Partitioning Into Minimum Number Of Deci-Binary Numbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Medium
+{
+    /// <summary>
+    /// https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
+    /// 
+    /// Case 1:
+    /// Input: n = "32"
+    /// Output: 3
+    /// Explanation: 10 + 11 + 11 = 32
+    /// 
+    /// Case 2:
+    /// Input: n = "82734"
+    /// Output: 8
+    /// 
+    /// </summary>
+    class _1689_Partitioning_Into_Minimum_Number_Of_Deci_Binary_Numbers
+    {
+        public int MinPartitions(string n)
+        {
+            return n.ToCharArray().Select(c => Convert.ToInt32(c.ToString())).Max();
+        }
+    }
+}

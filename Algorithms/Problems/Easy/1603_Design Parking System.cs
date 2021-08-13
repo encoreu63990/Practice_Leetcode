24a47195c8d828d9a9736c9fcf9a5d414a836989
劉品均 <encoreu63990@gmail.com>
2021-08-14 00:21:43 +0800

    1603_Design Parking System

---
+++ Algorithms/Problems/Easy/1603_Design Parking System.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/design-parking-system/
+    /// </summary>
+    class _1603_Design_Parking_System
+    {
+        public class ParkingSystem
+        {
+            private int[] parkingLot;
+
+            public ParkingSystem(int big, int medium, int small)
+            {
+                parkingLot = new int[4] { 0, big, medium, small };
+            }
+
+            public bool AddCar(int carType)
+            {
+                parkingLot[carType] -= 1;
+                return parkingLot[carType] >= 0 ? true : false;
+            }
+        }
+    }
+}

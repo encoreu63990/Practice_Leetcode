9bd4f443fb7e3f9a47ab0a7448c9cd0209a5db81
劉品均 <encoreu63990@gmail.com>
2021-08-13 23:42:10 +0800

    1431_Kids With the Greatest Number of Candies

---
+++ Algorithms/Problems/Easy/1431_Kids With the Greatest Number of Candies.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+    /// </summary>
+    class _1431_Kids_With_the_Greatest_Number_of_Candies
+    {
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            List<bool> result = new List<bool>();
+            int max = candies.Max();
+            foreach(int candy in candies)
+            {
+                result.Add(candy + extraCandies >= max ? true : false);
+            }
+            return result;
+        }
+    }
+}

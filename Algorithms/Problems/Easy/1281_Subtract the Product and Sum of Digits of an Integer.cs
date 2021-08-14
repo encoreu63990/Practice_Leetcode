f089b11d28fb06b2c67940156ee4567243201d4f
劉品均 <encoreu63990@gmail.com>
2021-08-14 18:21:10 +0800

    1281_Subtract the Product and Sum of Digits of an Integer

---
+++ Algorithms/Problems/Easy/1281_Subtract the Product and Sum of Digits of an Integer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+    /// </summary>
+    class _1281_Subtract_the_Product_and_Sum_of_Digits_of_an_Integer
+    {
+        public int SubtractProductAndSum(int n)
+        {
+            var product = 1;
+            var sum = 0;
+            while (n != 0)
+            {
+                var remainder = n % 10;
+                product *= remainder;
+                sum += remainder;
+                n /= 10;
+            }
+            return product - sum;
+        }
+    }
+}

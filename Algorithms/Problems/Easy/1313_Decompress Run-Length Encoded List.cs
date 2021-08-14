dfa9d1e1425486504ccc94b05815c3df7c1fb6bc
劉品均 <encoreu63990@gmail.com>
2021-08-14 18:28:31 +0800

    1313_Decompress Run-Length Encoded List

---
+++ Algorithms/Problems/Easy/1313_Decompress Run-Length Encoded List.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/decompress-run-length-encoded-list/
+    /// </summary>
+    class _1313_Decompress_Run_Length_Encoded_List
+    {
+        public int[] DecompressRLElist(int[] nums)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 1; i < nums.Length; i += 2)
+            {
+                for (int j = 0; j < nums[i - 1]; j++)
+                {
+                    result.Add(nums[i]);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}

a371036b20425cef04276d1d46e220cc4d8a1e09
劉品均 <encoreu63990@gmail.com>
2021-08-14 18:05:22 +0800

    1528_Shuffle String

---
+++ Algorithms/Problems/Easy/1528_Shuffle String.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/shuffle-string/
+    /// </summary>
+    class _1528_Shuffle_String
+    {
+        public string RestoreString(string s, int[] indices)
+        {
+            var length = indices.Length;
+            var array = new char[s.Length];
+            for (int i = 0; i < length; i++)
+            {
+                array[indices[i]] = s[i];
+            }
+            return string.Join("", array);
+        }
+    }
+}

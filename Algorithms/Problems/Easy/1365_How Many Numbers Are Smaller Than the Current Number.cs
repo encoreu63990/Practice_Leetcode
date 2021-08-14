0c2517e3d48e59bef42794fa20f2ac8caeec858a
劉品均 <encoreu63990@gmail.com>
2021-08-14 17:02:48 +0800

    1365_How Many Numbers Are Smaller Than the Current Number

---
+++ Algorithms/Problems/Easy/1365_How Many Numbers Are Smaller Than the Current Number.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Easy
+{
+    /// <summary>
+    /// /https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+    /// </summary>
+    class _1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number
+    {
+        public int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            return nums.Select(c => nums.Where(v => c > v).Count()).ToArray();
+        }
+    }
+}

6b9dcb3d72e56b44dd41dc1bf2c564ed302e66a6
劉品均 <encoreu63990@gmail.com>
2021-08-16 10:51:22 +0800

    1389_Create Target Array in the Given Order

---
+++ Algorithms/Problems/Easy/1389_Create Target Array in the Given Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/create-target-array-in-the-given-order/
+    /// </summary>
+    class _1389_Create_Target_Array_in_the_Given_Order
+    {
+        public int[] CreateTargetArray(int[] nums, int[] index)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < index.Length; i++)
+            {
+                result.Insert(index[i], nums[i]);
+            }
+            return result.ToArray();
+        }
+    }
+}

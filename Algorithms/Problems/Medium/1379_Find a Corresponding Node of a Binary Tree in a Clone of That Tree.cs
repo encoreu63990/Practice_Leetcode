54c13a4477021b6558b3004c3ac825a6c3686107
劉品均 <encoreu63990@gmail.com>
2021-08-16 16:40:46 +0800

    1379_Find a Corresponding Node of a Binary Tree in a Clone of That Tree

---
+++ Algorithms/Problems/Medium/1379_Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Medium
+{
+    /// <summary>
+    /// https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/
+    /// </summary>
+    class _1379_Find_a_Corresponding_Node_of_a_Binary_Tree_in_a_Clone_of_That_Tree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+        public TreeNode GetTargetCopy(TreeNode original, TreeNode cloned, TreeNode target)
+        {
+            if (original == null)
+                return null;
+
+            if (original == target)
+                return cloned;
+
+            return GetTargetCopy(original.left, cloned.left, target) 
+                   ?? GetTargetCopy(original.right, cloned.right, target);
+        }
+    }
+}

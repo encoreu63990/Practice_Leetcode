32f70dafe076240b0b7fcc42e22da65095542095
劉品均 <encoreu63990@gmail.com>
2021-08-16 10:54:10 +0800

    1678_Goal Parser Interpretation

---
+++ Algorithms/Problems/Easy/1678_Goal Parser Interpretation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/goal-parser-interpretation/
+    /// </summary>
+    class _1678_Goal_Parser_Interpretation
+    {
+        public string Interpret(string command)
+        {
+            var result = command.Replace("()", "o");
+            result = result.Replace("(al)", "al");
+            return result;
+        }
+    }
+}

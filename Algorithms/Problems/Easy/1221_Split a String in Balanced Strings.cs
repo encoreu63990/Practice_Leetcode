37f46cabeb3d7e903afc96642d570b9f2177fda3
劉品均 <encoreu63990@gmail.com>
2021-08-17 12:45:49 +0800

    1221_Split a String in Balanced Strings

---
+++ Algorithms/Problems/Easy/1221_Split a String in Balanced Strings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/split-a-string-in-balanced-strings/
+    /// </summary>
+    class _1221_Split_a_String_in_Balanced_Strings
+    {
+        public int BalancedStringSplit(string s)
+        {
+            var count = 0;
+            var balancedNumber = 0;
+
+            foreach (char c in s)
+            {
+                if (c == 'L')
+                    balancedNumber++;
+                else
+                    balancedNumber--;
+
+                if (balancedNumber == 0)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}

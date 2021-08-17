a13293f0c064e86cccf6b2345eba2573918f3494
劉品均 <encoreu63990@gmail.com>
2021-08-17 11:26:37 +0800

    1773_Count Items Matching a Rule

---
+++ Algorithms/Problems/Easy/1773_Count Items Matching a Rule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/count-items-matching-a-rule/
+    /// </summary>
+    class _1773_Count_Items_Matching_a_Rule
+    {
+        public int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+        {
+            var keys = new List<string> { "type", "color", "name" };
+            var index = keys.IndexOf(ruleKey);
+            return items.Where(list => list[index] == ruleValue).Count();
+        }
+    }
+}

f6132c15e60570460c6020875363198dbcd9f12c
劉品均 <encoreu63990@gmail.com>
2021-08-17 15:53:25 +0800

    1486_XOR Operation in an Array

---
+++ Algorithms/Problems/Easy/1486_XOR Operation in an Array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Easy
+{
+    class _1486_XOR_Operation_in_an_Array
+    {
+        public int XorOperation(int n, int start)
+        {
+            int result = 0;
+
+            for (int i=0; i<n; i++)
+            {
+                result ^= start + 2 * i;
+            }
+
+            return result;
+        }
+    }
+}

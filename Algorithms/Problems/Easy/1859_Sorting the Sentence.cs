22c1ea930eb4bf985e64b21d6c152ce0a5ec20b5
劉品均 <encoreu63990@gmail.com>
2021-08-18 11:05:24 +0800

    1859_Sorting the Sentence

---
+++ Algorithms/Problems/Easy/1859_Sorting the Sentence.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/sorting-the-sentence/
+    /// </summary>
+    class _1859_Sorting_the_Sentence
+    {
+        public string SortSentence(string s)
+        {
+            var sortedSentence = s.Split(' ').Select(st =>
+            new
+            {
+                word = st.Substring(0, st.Length - 1),
+                number = Convert.ToInt32(st.Substring(st.Length - 1, 1))
+            }).OrderBy(c => c.number).Select(c => c.word).Aggregate((l, r) => l + " " + r);
+
+            return sortedSentence;
+        }
+    }
+}

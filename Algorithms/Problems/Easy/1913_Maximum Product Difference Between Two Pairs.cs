9f263bd0fffd80d0accb3c6c8420d8919a621950
劉品均 <encoreu63990@gmail.com>
2021-08-19 11:36:39 +0800

    1913_Maximum Product Difference Between Two Pairs

---
+++ Algorithms/Problems/Easy/1913_Maximum Product Difference Between Two Pairs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
+    /// </summary>
+    class _1913_Maximum_Product_Difference_Between_Two_Pairs
+    {
+        public int MaxProductDifference(int[] nums)
+        {
+            var array = nums.OrderBy(c => c).ToArray();
+            var max = array[array.Length - 1] * array[array.Length - 2];
+            var min = array[0] * array[1];
+            return max - min;
+        }
+    }
+}

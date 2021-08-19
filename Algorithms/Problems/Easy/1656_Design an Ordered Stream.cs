50a86ee402a420acb940634fc6c12e562d5d27a9
劉品均 <encoreu63990@gmail.com>
2021-08-19 10:51:38 +0800

    1656_Design an Ordered Stream

---
+++ Algorithms/Problems/Easy/1656_Design an Ordered Stream.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/design-an-ordered-stream/
+    /// </summary>
+    class _1656_Design_an_Ordered_Stream
+    {
+        public class OrderedStream
+        {
+            string[] _array = null;
+            int counter;
+
+            public OrderedStream(int n)
+            {
+                _array = new string[n + 1];
+                counter = 1;
+            }
+
+            public IList<string> Insert(int idKey, string value)
+            {
+                _array[idKey] = value;
+                var result = new List<string>();
+
+                while (_array[counter] != null)
+                    result.Add(_array[counter++]);
+
+                return result;
+            }
+        }
+    }
+}

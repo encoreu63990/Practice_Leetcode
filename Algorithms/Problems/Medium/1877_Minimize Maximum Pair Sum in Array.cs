550d9655cca2470d6c72dccaa332212df4ae58ae
劉品均 <encoreu63990@gmail.com>
2021-08-20 16:20:22 +0800

    1877_Minimize Maximum Pair Sum in Array

---
+++ Algorithms/Problems/Medium/1877_Minimize Maximum Pair Sum in Array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Medium
+{
+    /// <summary>
+    /// https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/
+    /// </summary>
+    class _1877_Minimize_Maximum_Pair_Sum_in_Array
+    {
+        public int MinPairSum(int[] nums)
+        {
+            nums = nums.OrderBy(c => c).ToArray();
+            var max = nums[0] + nums[nums.Length - 1];
+            for (int i = 1, j = nums.Length - 2; j > i; i++, j--)
+            {
+                max = Math.Max(nums[i] + nums[j], max);
+            }
+            return max;
+        }
+    }
+}

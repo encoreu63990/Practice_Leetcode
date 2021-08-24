adff090a31c31a5547b30765b0d332d04263a1df
劉品均 <encoreu63990@gmail.com>
2021-08-24 18:09:07 +0800

    921_Minimum Add to Make Parentheses Valid

---
+++ Algorithms/Problems/Medium/921_Minimum Add to Make Parentheses Valid.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Medium
+{
+    /// <summary>
+    /// https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+    /// https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/discuss/1423023/C-Solution
+    /// </summary>
+    class _921_Minimum_Add_to_Make_Parentheses_Valid
+    {
+        public int MinAddToMakeValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var c in s)
+            {
+                if (c == ')' && stack.Any() && stack.Peek() == '(')
+                    stack.Pop();
+                else
+                    stack.Push(c);
+            }
+
+            return stack.Count;
+        }
+    }
+}

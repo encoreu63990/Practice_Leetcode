cb2fd45e836e667353e643392545a21b18055245
劉品均 <encoreu63990@gmail.com>
2021-08-24 11:42:54 +0800

    1381_Design a Stack With Increment Operation

---
+++ Algorithms/Problems/Medium/1381_Design a Stack With Increment Operation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Medium
+{
+    /// <summary>
+    /// https://leetcode.com/problems/design-a-stack-with-increment-operation/
+    /// https://leetcode.com/problems/design-a-stack-with-increment-operation/discuss/1422407/C-Solution
+    /// </summary>
+    class _1381_Design_a_Stack_With_Increment_Operation
+    {
+        public class CustomStack
+        {
+            private int[] _array;
+            private int index;
+
+
+            public CustomStack(int maxSize)
+            {
+                _array = new int[maxSize];
+                index = -1;
+            }
+
+            public void Push(int x)
+            {
+                if (index + 1 < _array.Length)
+                    _array[++index] = x;
+            }
+
+            public int Pop()
+            {
+                if (index >= 0)
+                    return _array[index--];
+                return -1;
+            }
+
+            public void Increment(int k, int val)
+            {
+                for (int i = 0; i < Math.Min(k, _array.Length); i++)
+                    _array[i] += val;
+            }
+        }
+    }
+}

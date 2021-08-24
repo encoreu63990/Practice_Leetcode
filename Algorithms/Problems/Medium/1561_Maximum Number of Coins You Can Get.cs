cbbc0fbb060faa2f0eca711c9563b9b7613f221c
劉品均 <encoreu63990@gmail.com>
2021-08-24 09:45:12 +0800

    1561_Maximum Number of Coins You Can Get

---
+++ Algorithms/Problems/Medium/1561_Maximum Number of Coins You Can Get.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Medium
+{
+    /// <summary>
+    /// https://leetcode.com/problems/maximum-number-of-coins-you-can-get/
+    /// </summary>
+    class _1561_Maximum_Number_of_Coins_You_Can_Get
+    {
+        public int MaxCoins(int[] piles)
+        {
+            Array.Sort(piles);
+            Array.Reverse(piles);
+
+            var result = 0;
+
+            for (int i = 0; i < piles.Length / 3; i++)
+            {
+                result += piles[1 + i * 2];
+            }
+
+            return result;
+        }
+    }
+}

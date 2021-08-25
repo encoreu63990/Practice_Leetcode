0550936f19e36a9b13038502beedd3c4025d9f7f
劉品均 <encoreu63990@gmail.com>
2021-08-25 17:57:44 +0800

    1347_Minimum Number of Steps to Make Two Strings Anagram

---
+++ Algorithms/Problems/Medium/1347_Minimum Number of Steps to Make Two Strings Anagram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Medium
+{
+    /// <summary>
+    /// https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/
+    /// </summary>
+    class _1347_Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram
+    {
+        public int MinSteps(string s, string t)
+        {
+            int[] frequency = new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                frequency[t[i] - 'a']++;
+                frequency[s[i] - 'a']--;
+            }
+
+            var result = frequency.Where(c => c > 0).Sum();
+            return result;
+        }
+    }
+}

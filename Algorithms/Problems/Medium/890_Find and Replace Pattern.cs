5e03129010745840e93e7b12f3eaa940dd22a79f
劉品均 <encoreu63990@gmail.com>
2021-08-25 14:05:47 +0800

    890_Find and Replace Pattern

---
+++ Algorithms/Problems/Medium/890_Find and Replace Pattern.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Medium
+{
+    /// <summary>
+    /// https://leetcode.com/problems/find-and-replace-pattern/
+    /// 890_Find and Replace Pattern
+    /// </summary>
+    class _890_Find_and_Replace_Pattern
+    {
+        public IList<string> FindAndReplacePattern(string[] words, string pattern)
+        {
+            var result = new List<string>();
+            var replacedPattern = Replace(pattern);
+
+            foreach (var word in words)
+            {
+                var replacedWord = Replace(word);
+                if (Enumerable.SequenceEqual(replacedPattern, replacedWord))
+                    result.Add(word);
+            }
+
+            return result;
+        }
+
+        public List<int> Replace(string word)
+        {
+            List<int> list = new List<int>();
+            List<char> map = new List<char>();
+
+            foreach (var c in word)
+            {
+                if (!map.Contains(c))
+                    map.Add(c);
+
+                list.Add(map.IndexOf(c));
+            }
+
+            return list;
+        }
+    }
+}

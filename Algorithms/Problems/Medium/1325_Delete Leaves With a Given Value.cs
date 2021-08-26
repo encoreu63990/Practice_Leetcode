7fee50ec860042e432d548a115d8a26ecb11bdd2
劉品均 <encoreu63990@gmail.com>
2021-08-26 15:51:48 +0800

    1325_Delete Leaves With a Given Value

---
+++ Algorithms/Problems/Medium/1325_Delete Leaves With a Given Value.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Problems.Medium
+{
+    /// <summary>
+    /// https://leetcode.com/problems/delete-leaves-with-a-given-value/
+    /// </summary>
+    class _1325_Delete_Leaves_With_a_Given_Value
+    {
+        public TreeNode RemoveLeafNodes(TreeNode root, int target)
+        {
+            if (root == null)
+                return null;
+
+            root.left = RemoveLeafNodes(root.left, target);
+            root.right = RemoveLeafNodes(root.right, target);
+
+            if (root.val == target && root.left == null && root.right == null)
+                return null;
+
+            return root;
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+    }
+}
